1238405f10275455b43ebac999785aab23286e57
Анастасия Вельдина <nastya.rkc@yandex.ru>
2022-12-04 21:47:39 +0300

    Code style:practical

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from random import randint
+
 from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
     """Генерирует количество очков атаки.
-    
+
     Возвращает строковое сообщение о проведённой атаке.
     """
     if char_class == 'warrior':
@@ -117,6 +118,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
